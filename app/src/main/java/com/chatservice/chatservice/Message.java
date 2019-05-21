75e7f66fb647898314eac27ba7cc23d439a228fa
reema abo ajeeb <reemaa199622@gmail.com>
2019-05-21 14:47:41 +0300

    Initial commit

---
+++ app/src/main/java/com/chatservice/chatservice/Message.java
@@ -0,0 +1,16 @@
+package com.chatservice.chatservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Message extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.message);
+
+
+
+    }
+}
